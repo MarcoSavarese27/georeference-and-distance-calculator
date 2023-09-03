45bbb125defec5f10757e5c0d3c75de6979483a7
Marco <marcosavarese@live.it>
2023-09-03 18:45:20 +0200

    api updated, db_interaction.py improved, with a first version of a ttl

--- georeference/db_interaction.py
+++ georeference/db_interaction.py
@@ -8,17 +8,22 @@ def retrieve_data(add):
     if res is None:
         addressToGeocode = f'{add["address"]} {add["area"]} {add["district"]} {add["postalCode"]} {add["region"]}'
         geocode = geocoding_result(addressToGeocode)
-        
+        num = cur.execute("SELECT COUNT(*) FROM resolutions").fetchone()[0]
+        print(num)
         if geocode is None:
             return None
-        
-        cur.execute(f'INSERT OR IGNORE INTO resolutions VALUES (\"{address}\", \"{geocode["address"]}\", {geocode["latitude"]}, {geocode["longitude"]})')
+ 
+        if num == 30:
+            cur.execute('DELETE FROM resolutions WHERE access_counter = (SELECT MIN(access_counter) FROM resolutions) LIMIT 1')
+
+        cur.execute(f'INSERT OR IGNORE INTO resolutions VALUES (\"{address}\", \"{geocode["address"]}\", {geocode["latitude"]}, {geocode["longitude"]}, 1)')
         conn.commit()
         conn.close()
         return geocode
     
-    
+    cur.execute(f"UPDATE resolutions SET access_counter = access_counter + 1 WHERE address LIKE '{address}'")
     resolution = {"address": res[1], "latitude": res[2], "longitude": res[3]}
+    conn.commit()
     conn.close()
     return resolution
 
@@ -27,4 +32,5 @@ def get_db_connection():
     conn = sqlite3.connect('db/database.db')
     cur = conn.cursor()
     return conn, cur
+    
 
